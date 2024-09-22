d61a65f0cd12b34d99505629d649d6d917ad6d3a
unicoder11 <unicoder11@gmail.com>
2024-09-22 20:58:03 -0300

    prisma4

--- src/app/api/onboarding.ts
+++ src/app/api/onboarding.ts
@@ -1,12 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv';
+import ws from 'ws';
+
+dotenv.config();
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+const prisma = new PrismaClient({ adapter });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // if (req.method !== 'POST') {
-  //   return res.status(405).json({ message: 'Método não permitido' });
-  // }
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Método não permitido' });
+  }
 
   try {
     const { nome, cpf, documentoFoto, selfie } = req.body;
