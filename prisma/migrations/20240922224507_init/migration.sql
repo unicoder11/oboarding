d577d566690efa2c86bc4e0220883445518b4d32
unicoder11 <unicoder11@gmail.com>
2024-09-23 12:43:35 -0300

    migrations

--- prisma/migrations/20240922224507_init/migration.sql
+++ prisma/migrations/20240922224507_init/migration.sql
@@ -3,8 +3,8 @@ CREATE TABLE "Onboarding" (
     "id" SERIAL NOT NULL,
     "nome" TEXT NOT NULL,
     "cpf" TEXT NOT NULL,
-    "documentoFoto" TEXT NOT NULL,
-    "selfie" TEXT NOT NULL,
+    "documentoFoto" TEXT,
+    "selfie" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Onboarding_pkey" PRIMARY KEY ("id")
